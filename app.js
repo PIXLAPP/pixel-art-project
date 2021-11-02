cd8e808a4d4f0b07075531e0ca19b5179df345d3
fora-heims <foraheims@gmail.com>
2021-11-02 15:10:32 -0700

    The pencil tool was added and event listeners added to each div. The Pencil works, for single pixels at a time.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import { createImage, renderImage } from './utils.js';
 
-const testCanvas = createImage('coolTitle', 10, 10);
-renderImage(testCanvas);
+const selectedTool = document.querySelector('input[type=radio]:checked');
 
+const currentCanvas = createImage('coolTitle', 10, 10);
+renderImage(currentCanvas);
+const canvasDivs = document.querySelectorAll('.pixel-div');
+console.log(canvasDivs);
+
+for (let div of canvasDivs) {
+    div.addEventListener('click', () => {
+        div.style.backgroundColor = 'pink';
+    });
+}
