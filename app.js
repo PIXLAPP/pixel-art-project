d84f8ae27d02fc65c5fadb69261e041e832cedc5
fora-heims <foraheims@gmail.com>
2021-11-02 15:53:15 -0700

    Eraser feature added and functional. So much excite

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import { createImage, renderImage } from './utils.js';
 
-const selectedTool = document.querySelector('input[type=radio]:checked');
+
 
 const currentCanvas = createImage('coolTitle', 10, 10);
 renderImage(currentCanvas);
 const canvasDivs = document.querySelectorAll('.pixel-div');
-console.log(canvasDivs);
 
-for (let div of canvasDivs) {
-    div.addEventListener('click', () => {
-        div.style.backgroundColor = 'pink';
+const eraserBackgroundCanvas = createImage('eraser background', 10, 10);
+const eraserColorArray = eraserBackgroundCanvas.colors;
+
+for (let i = 0; i < canvasDivs.length; i++) {
+    canvasDivs[i].addEventListener('click', () => {
+        const selectedTool = document.querySelector('input[type=radio]:checked');
+        // const originalBackground = canvasDivs[i].style.backgroundColor;
+        if (selectedTool.id === 'pencil') {
+            canvasDivs[i].style.backgroundColor = 'black';
+        } else if (selectedTool.id === 'eraser') {
+            canvasDivs[i].style.backgroundColor = `#${eraserColorArray[i]}`;
+        }
     });
 }
