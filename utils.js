8ea9a8210aac47966313c2acd2de0b6010024bad
fora-heims <foraheims@gmail.com>
2021-11-03 20:56:10 -0700

    I definitely should have commited more. Refactored a lot. Fixed bug in rainbow pencil. Hover an pixels is visible. Hover on gallery is visible. Gallery renders correct dimensions. Choose size of canvas feature functional. Canvas and pixel sizes are dynamic based in height and width. Requires image title to start. Will not let you use an image title that already exists in the lirary. Requires height and width to be below 50.

--- utils.js
+++ utils.js
@@ -14,8 +14,8 @@ export function titleKey(imageTitle) {
     return noSpaceTitle;
 }
 
-export function createImage(title, height, width) {
-    let area = height * width;
+export function createImage(title, heightInput, widthInput) {
+    let area = heightInput * widthInput;
     // let halfArea = area / 2;
     let thirdArea = Math.ceil(area / 3);
     let colorArray = [];
@@ -26,8 +26,8 @@ export function createImage(title, height, width) {
     
     let newImage = {
         title: title,
-        height: 10,
-        width: 10,
+        height: heightInput,
+        width: widthInput,
         colors: colorArray,
     };
     return newImage;    
@@ -40,12 +40,30 @@ export function updateImage(imageObject, colorArray) {
 
 export function renderImage(imageObject) {
     const canvas = document.getElementById('canvas');
+    canvas.style.gridTemplateColumns = `repeat(${imageObject.width}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${imageObject.height}, 1fr)`;
     const colorArray = imageObject.colors;
     const area = imageObject.height * imageObject.width;
     for (let i = 0; i < area; i++) {
         const pixel = document.createElement('div');
         pixel.style.backgroundColor = colorArray[i];
         pixel.classList.add('pixel-div');
+        if (imageObject.height === imageObject.width) {
+            pixel.style.width = `${Math.round(500 / imageObject.height)}px)`;
+            pixel.style.height = `${Math.round(500 / imageObject.height)}px`;
+        } else if (imageObject.height > imageObject.width) {
+            pixel.style.width = `${Math.round(500 / imageObject.height)}px`;
+            pixel.style.height = `${Math.round(500 / imageObject.height)}px`;
+            canvas.style.height = '500px';
+            canvas.style.width = `${Math.round(imageObject.width * (500 / imageObject.height))}px`;
+        } else {
+            pixel.style.width = `${Math.round(500 / imageObject.width)}px`;
+            pixel.style.height = `${Math.round(500 / imageObject.width)}px`;
+            canvas.style.width = '500px';
+            canvas.style.height = `${Math.round(imageObject.height * (500 / imageObject.width))}px`;
+        }
+        
+
         canvas.append(pixel);  
     }
 }
