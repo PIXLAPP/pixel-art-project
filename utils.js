c0a0aeee0f555df8bc1217133bd695bfcb20609e
fora-heims <foraheims@gmail.com>
2021-11-03 23:46:14 -0700

    Fixed the generate image so that the grey and white background created will correcly alternate on each row, so now we don't have to use three grey colors. Styled the inputs on the home page for title, height, and width.

--- utils.js
+++ utils.js
@@ -14,16 +14,45 @@ export function titleKey(imageTitle) {
     return noSpaceTitle;
 }
 
+function isEven(num) {
+    return num % 2 === 0;
+}
+
 export function createImage(title, heightInput, widthInput) {
     let area = heightInput * widthInput;
-    // let halfArea = area / 2;
-    let thirdArea = Math.ceil(area / 3);
+    let halfArea = Math.ceil(area / 2);
+    // let thirdArea = Math.ceil(area / 3);
     let colorArray = [];
+    let x = widthInput / 2;
+    let y = 1;
+    if (isEven(widthInput) === true) {
+        
 
-    for (let i = 0; i < thirdArea; i++) {  
-        colorArray.push('rgb(216, 216, 217)', 'rgb(254, 255, 254)', 'rgb(243, 243, 244)',); 
+        for (let i = 0; i < halfArea; i++) {  
+            if (isEven(y) === true) {
+                colorArray.push('rgb(243, 243, 244)', 'rgb(216, 216, 217)');
+                x--;
+                if (x === 0){
+                    y++;
+                    x = widthInput / 2;
+                }
+            } else {
+                colorArray.push('rgb(216, 216, 217)', 'rgb(243, 243, 244)');
+                x--;
+                if (x === 0){
+                    y++;
+                    x = widthInput / 2;
+                }
+            }
+        }
+    } else { 
+        for (let i = 0; i < halfArea; i++) {  
+            colorArray.push('rgb(216, 216, 217)', 'rgb(243, 243, 244)'); 
+        }
     }
-    
+
+            // colorArray.push('rgb(216, 216, 217)', 'rgb(254, 255, 254)', 'rgb(243, 243, 244)',); 
+
     let newImage = {
         title: title,
         height: heightInput,
